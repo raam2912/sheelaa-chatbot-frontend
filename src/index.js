073d5b528a0e0b2219a2f899afc50a311f6a1a45
raam2912 <raamnaraayanan775@gmail.com>
2025-06-30 22:40:59 +0530

    FRONTEND ON GHPAGE-0.1

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client'; // Use createRoot for React 18
 import './index.css'; // Your main CSS (Tailwind will be processed here)
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
